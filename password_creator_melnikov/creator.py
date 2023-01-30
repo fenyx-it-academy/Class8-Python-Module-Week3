e27f70d128d3216c5c9003ceacec3674909983dc
meldanil <111883970+meldanil@users.noreply.github.com>
2023-01-30 18:20:47 +0100

    Changes made to the password generator to make it more relevant.

--- password_creator_melnikov/creator.py
+++ password_creator_melnikov/creator.py
@@ -2,14 +2,18 @@ import tkinter as tk
 import passfunc as pf
 from tkinter import messagebox
 
+def getint(x):
+    return int(x.get())
+
 def clicked():
-    messagebox.showinfo(message=f'There is your safe password \n {pf.generate_password(length_p.get())} \n Copy it in the safe place.')
+    messagebox.showinfo(
+        message=f'''There is your safe password \n \
+            {pf.generate_password(getint(length_p), numlet=getint(number_let), char=chk_char.get())} \n \
+            Copy it in the safe place.''')
 
 screen = tk.Tk()
 title = screen.title('Password Creator')
-screen.geometry('400x230')
-# canvas = tk.Canvas(screen, width=300, height=500)
-# canvas.pack()
+screen.geometry('400x270')
 
 
 #label
@@ -28,19 +32,25 @@ label1.grid(row=0, column=0, sticky=tk.W+tk.E)
 length_p = tk.Entry(labelframe, font=('Roboto', 12), width=10)
 length_p.grid(row=0, column=1, sticky=tk.W+tk.E)
 
+label2 = tk.Label(labelframe, text='Number of letters', font=('Roboto', 12))
+label2.grid(row=1, column=0, sticky=tk.W+tk.E)
 
-# label2 = tk.Label(labelframe, text='Password count', font=('Roboto', 12))
-# label2.grid(row=1, column=0, sticky=tk.W+tk.E)
+number_let = tk.Entry(labelframe, font=('Roboto', 12), width=10)
+number_let.grid(row=1, column=1, sticky=tk.W+tk.E)
 
-# count = tk.Entry(labelframe, font=('Roboto', 12), width=10)
-# count.grid(row=1, column=1, sticky=tk.W+tk.E)
-
+chk_char = tk.BooleanVar()
+chk_char.set(True)
+ischaruse = tk.Checkbutton(labelframe, text='Use special characters', var=chk_char)
+ischaruse.grid(row=2, column=1)
 labelframe.pack()
 
 #button frame
 buttonframe = tk.Frame(screen)
+
 submit_button = tk.Button(buttonframe, text='Submit', font=('Roboto', 14), command=clicked)
-submit_button.pack(fill='x', pady=20)
+submit_button.config(height= 20, width=26)
+submit_button.pack(pady=20)
+
 buttonframe.pack()
 
 screen.mainloop()
