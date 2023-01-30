be634eeb34986a56da79c7f49d3322fcc02269f7
Ammar Esam <ammar.alsharee1@gmail.com>
2023-01-30 23:58:24 +0100

    Update Bonus_1.py

--- Bonus_1.py
+++ Bonus_1.py
@@ -1,8 +1,4 @@
-# Program to filter out only the even items from a list
-my_list = [1, 5, 4, 6, 8, 11, 3, 12]
-
-even_list = list(filter(lambda x: (x % 2 == 0), my_list))
-odd_list = list(filter(lambda x: (x % 2 != 0), my_list))
-
-print(even_list)
-print(odd_list)
+dict = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max',
+        'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+print('Sorting the List of dictionaries based on make:',sorted(dict, key=lambda i: i['make']))
+print('Sorting the List of dictionaries based on color:',sorted(dict, key=lambda i: i['color']))
